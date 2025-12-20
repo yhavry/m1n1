c29d89e8acd5a5c040d46ccbd3af56326ee6ea51
Nick Chan <towinchenmi@gmail.com>
2025-12-21 00:48:25 +0800

    looks like flags are not needed for single lba read

--- src/nvme.c
+++ src/nvme.c
@@ -602,12 +602,8 @@ bool nvme_read(u32 nsid, u64 lba, void *buffer)
     cmd.prp1 = (u64)buffer_addr;
     cmd.cdw10 = lba;
     cmd.cdw11 = lba >> 32;
-    if (nvme_type == NVME_TYPE_T8015)
-        cmd.cdw12 = 0; // 4096 bytes
-    else
-        cmd.cdw12 = 1; // 4096 bytes
-    if (nvme_type == NVME_TYPE_T8015)
-        cmd.flags = 0x20;
+    if (nvme_type == NVME_TYPE_T8103)
+        cmd.cdw12 = 1; // 4096 bytes (1-indexed)
 
     return nvme_exec_command(&ioq, &cmd, NULL);
 }
