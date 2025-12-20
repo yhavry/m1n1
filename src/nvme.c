bfaf74ca8b1c2daecf7793a562ae3991ddf0a622
Nick Chan <towinchenmi@gmail.com>
2025-12-21 01:47:37 +0800

    cdw12 should have always been 0

--- src/nvme.c
+++ src/nvme.c
@@ -602,8 +602,6 @@ bool nvme_read(u32 nsid, u64 lba, void *buffer)
     cmd.prp1 = (u64)buffer_addr;
     cmd.cdw10 = lba;
     cmd.cdw11 = lba >> 32;
-    if (nvme_type == NVME_TYPE_T8103)
-        cmd.cdw12 = 1; // 4096 bytes (1-indexed)
 
     return nvme_exec_command(&ioq, &cmd, NULL);
 }
