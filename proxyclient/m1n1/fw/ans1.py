3e3be76752a96c0e5535fe5e45007e240f12247d
Nick Chan <towinchenmi@gmail.com>
2026-07-04 03:37:01 +0800

    ns setup closer to iboot

--- proxyclient/m1n1/fw/ans1.py
+++ proxyclient/m1n1/fw/ans1.py
@@ -27,6 +27,7 @@ class ANS_SetBase(ANS_Message):
     CMD = 0
 
 class ANS_Cmd(ANS_Message):
+    UNK = 31, 24
     ARG_2 = 19, 16 # (NSID * 2) & 0xf
     ARG_3 = 15, 12 # (NSID * 3) & 0xf
     NSID = 7, 4
@@ -47,10 +48,9 @@ class ANSEndpoint(AKFBaseEndpoint):
         self.mon = RegMonitor(u, ascii=True, bufsize=0x8000000)
         self.base = None
 
-    def ns_setup(self, ns):
-        arg2 = (ns * 2) & 0xf
-        arg3 = (ns * 3) & 0xf
-        self.send(ANS_Admin_Cmd(ARG_2=arg2, ARG_3=arg3, NSID=ns, IO=0, CMD=1))
+    def ns_setup(self, ns, arg2, arg3):
+        # UNK may be multiplier of ARG2 and ARG3
+        self.send(ANS_Admin_Cmd(ARG_2=arg2, ARG_3=arg3, NSID=ns, UNK=0x23, IO=0, CMD=1))
         self.akf.work()
         self.mon.poll()
 
@@ -70,8 +70,14 @@ class ANSEndpoint(AKFBaseEndpoint):
         self.send(ANS_SetBase(BASE=self.base, UNK=0x118, IO=0, CMD=0))
         self.akf.work()
         self.mon.poll()
-        for i in range(0, 8):
-            self.ns_setup(i)
+        self.ns_setup(0, 0, 0)
+        self.ns_setup(1, 2, 3)
+        self.ns_setup(2, 4, 6)
+        self.ns_setup(3, 6, 9)
+        self.ns_setup(4, 8, 0xc)
+        self.ns_setup(5, 0xa, 0xf)
+        self.ns_setup(6, 0xd, 2) # arg2 off by one ????
+        self.ns_setup(7, 0xf, 5)
 
     def stop(self):
         if self.base:
